23f5cea17eb4a73acb7ed73e7e8115cc04110387
ikbakkk <iqbalfirdaus05@gmail.com>
2023-01-14 21:34:49 +0800

    Add pricing

--- The Malaka/src/components/Preview.tsx
+++ The Malaka/src/components/Preview.tsx
@@ -6,7 +6,7 @@ function Preview() {
     <section id='preview' className='bg-[#EDFCFD] py-[2.5rem]'>
       <div className='flex flex-col items-center p-4 py-10 pt-10 font-dm-sans md:px-28'>
         <div className='flex max-w-4xl flex-col items-center space-y-4 text-center'>
-          <p className='rounded-full bg-[#C6F4F8] px-5 py-2 text-sm font-bold tracking-wide text-primary'>
+          <p className='rounded-full bg-[#C6F4F8] px-5 py-2 text-sm font-bold uppercase tracking-wide text-primary'>
             Pratinjau
           </p>
           <h2 className='text-4xl font-semibold text-dark'>
@@ -18,11 +18,11 @@ function Preview() {
             detail halaman buku tersebut dan membaca isinya.
           </p>
         </div>
-        <div className='flex w-full justify-between'>
-          <div className='basis-1/3'>
+        <div className='flex w-full flex-col md:justify-between lg:flex-row'>
+          <div className='lg:basis-1/3'>
             <img src={book} alt='/' />
           </div>
-          <div className='flex basis-2/3 flex-col text-center'>
+          <div className='flex flex-col text-center lg:basis-2/3'>
             <div className='flex w-full justify-center'>
               <div className='max-w-lg'>
                 <PreviewCarousel />
