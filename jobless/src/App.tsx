14038f430988c97dd7354731e5243bed4b9a8689
ikbakkk <iqbalfirdaus05@gmail.com>
2023-01-21 10:34:01 +0800

    initial commit

---
+++ jobless/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className=''>
+      <section className='w-10'>
+        <div className='scale-75 bg-black duration-300 hover:scale-100'></div>
+      </section>
+    </main>
+  )
+}
+
+export default App
